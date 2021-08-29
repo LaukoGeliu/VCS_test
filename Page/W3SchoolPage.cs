48c465484f82ff8caae7bcad8105108d9e48c722
LaukoGeliu <dziaugyte.gintare@gmail.com>
2021-08-29 22:26:00 +0300

    Second commit

---
+++ Page/W3SchoolPage.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automatinis1.Page
+{
+    class W3SchoolPage : BasePage
+    {
+        private const string AddressUrl = "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select_multiple";
+
+        private IWebElement cookiesWindow => Driver.FindElement(By.ClassName("sn-logo"));
+        private IWebElement closeCookiesButton => Driver.FindElement(By.Id("accept-choices"));
+        private SelectElement carsNamesDropdown => new SelectElement(Driver.FindElement(By.Id("cars")));
+        private IWebElement submitButton => Driver.FindElement(By.CssSelector("body>form>input[type=submit]"));
+        private IWebElement resultField => Driver.FindElement(By.ClassName("w3-container w3-large w3-border"));
+
+        public W3SchoolPage(IWebDriver webdriver) : base(webdriver) { }
+
+        public void NavigateToPage()
+        {
+            if (Driver.Url != AddressUrl)
+                Driver.Url = AddressUrl;
+        }
+
+        public void CloseCookies()
+        {
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+            wait.Until(d => cookiesWindow.Displayed);
+            closeCookiesButton.Click();
+        }
+
+        public void SelectCarsByValue(List<string> cars)
+        {
+            carsNamesDropdown.DeselectAll();
+            Actions action = new Actions(Driver);
+            action.KeyDown(Keys.Control);
+            IList<IWebElement> allOptions = carsNamesDropdown.Options;
+            foreach (string car in cars)
+            {
+                foreach (IWebElement option in allOptions)
+                    if (option.Text.Equals(car) && !option.Selected)
+                    {
+                        carsNamesDropdown.SelectByText(car);
+                    }
+            }
+            action.KeyUp(Keys.Control);
+            action.Build().Perform();
+        }
+
+        public void ClickSubmitButton()
+        { submitButton.Click(); }
+
+        public void VerifySelectedCarsResult(List<string> cars)
+        {
+            foreach (string car in cars)
+            {
+                Assert.IsTrue(resultField.Text.Contains(car), $"{car} car is not present");
+            }
+        }
+    }
+}
+
---
+++ Test/W3SchoolTest.cs
@@ -0,0 +1,47 @@
+﻿using Automatinis1.Page;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automatinis1.Test
+{
+    class W3SchoolTest
+    {
+        private static W3SchoolPage page;
+
+        [OneTimeSetUp]
+        public static void OneTimeSetUp()
+        {
+            IWebDriver chrome = new ChromeDriver();
+            page = new W3SchoolPage(chrome);
+            chrome.Manage().Window.Maximize();
+            chrome.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            page.NavigateToPage();
+            page.CloseCookies();
+        }
+
+        [OneTimeTearDown]
+        public static void OneTimeTearDown()
+        {
+            page.CloseBrowser();
+        }
+
+        [TestCase("volvo", TestName = "Test dropdown with one - Volvo -  value")]
+        [TestCase("saab", "opel", TestName = "Test dropdown with two - Saab, Opel - values")]
+        [TestCase("volvo", "saab", "opel", "audi", TestName = "Test dropdown with all - Volvo, Saab, Opel, Audi - values")]
+        public static void TestCarsDropdown(params string[] cars)
+        {
+
+            List<string> carsList = cars.ToList();
+            page.SelectCarsByValue(carsList);
+            page.ClickSubmitButton();
+            page.VerifySelectedCarsResult(carsList);
+        }
+    }
+    
+}
