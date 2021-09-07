0343601cc920e2c55918b96c4b625f075c2a4154
LaukoGeliu <89358405+LaukoGeliu@users.noreply.github.com>
2021-09-07 14:19:20 +0300

    Add files via upload

--- Test/ActiveTest.cs
+++ Test/ActiveTest.cs
@@ -1,47 +1,29 @@
-﻿using Automatinis1.Page;
+﻿using MembershopTest.Page;
 using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Automatinis1.Test
+namespace MembershopTest.Test
 {
-    class ActiveTest
+    public class ActiveTest : BaseTest
     {
-        private static ActivePage page;
-
-        [OneTimeSetUp]
-        public static void OneTimeSetUp()
+        [TestCase("1", "5", "13", "05", TestName = "13 km in 1 hour 5 min is 5 km/hour")]
+        public static void Test13KmIn1Hour5Min(string hour, string minutes, string distance, string resultMin)
         {
-            IWebDriver chrome = new ChromeDriver();
-            page = new ActivePage(chrome);
-
-            chrome.Manage().Window.Maximize();
-            chrome.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            chrome.Url = "https://www.active.com/fitness/calculators/pace";
-            page.CloseAdvertisement();
-        }
-
-        [OneTimeTearDown]
-        public static void OneTimeTearDown()
-        {
-            page.CloseBrowser();
-        }
-
-        [TestCase("1", "5", "13", "km", "per km", TestName = "13 km in 1 hour 5 min is 5 km/hour")]
-        public static void Test13KmIn1Hour5Min(string hour, string minutes, string distance, string distanceType, string speedTipe)
-        {
-            page.InsertRunningHours(hour);
-            page.InsertRunningMinutes(minutes);
-            page.InsertRunningDistance(distance);
-            page.SelectRunningDistanceType(distanceType);
-            page.SelectRunningSpeedPerHourType(speedTipe);
-            page.CountSpeedPerHour();
-
+            _activePage.NavigateToPage();
+            //_activePage.CloseAdvertisement();
+            _activePage.AcceptCookies();
+            _activePage.InsertRunningHours(hour);
+            _activePage.InsertRunningMinutes(minutes);
+            _activePage.InsertRunningDistance(distance);
+            _activePage.SelectRunningDistanceKm();
+            _activePage.SelectRunningSpeedPerHourTypeKmPerHour();
+            _activePage.CountPacePerHour();
+            _activePage.ValidatePaceResult(resultMin);
+            _activePage.Reset();
         }
     }
 }
