14bd851ef0ed630d348f4e103687d30a7476d564
LaukoGeliu <89358405+LaukoGeliu@users.noreply.github.com>
2021-09-07 14:19:50 +0300

    Add files via upload

--- Page/ActivePage.cs
+++ Page/ActivePage.cs
@@ -1,4 +1,6 @@
-﻿using OpenQA.Selenium;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
@@ -6,37 +8,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Automatinis1.Page
+namespace MembershopTest.Page
 {
     public class ActivePage : BasePage
     {
+        private const string PageAddress = "https://www.active.com/fitness/calculators/pace";
         private IWebElement popUpAdvertisement => Driver.FindElement(By.ClassName("virtual-events-modal__inner"));
         private IWebElement popUpAdvertisementCloseButton => Driver.FindElement(By.ClassName("virtual-events-modal__close"));
         //private IWebElement imageAdvertisement => Driver.FindElement(By.CssSelector("#img_anch_CL6kveG0yvICFchV4Aodq5cIjQ > img"));
         // private IWebElement imageAdvertisementCLoseButton => Driver.FindElement(By.CssSelector("#cbb > svg"));
-
         private IWebElement runningTimeHours => Driver.FindElement(By.Name("time_hours"));
-
-        internal void Quit()
-        {
-            throw new NotImplementedException();
-        }
-
         private IWebElement runningTimeMinutes => Driver.FindElement(By.Name("time_minutes"));
-
         private IWebElement runningDistance => Driver.FindElement(By.Name("distance"));
-        private SelectElement dropdownDistanceType => new SelectElement(Driver.FindElement(By.CssSelector("#calculator-pace > form > div:nth-child(3) > div > select")));
-
-        private SelectElement dropdownSpeedPerHourType => new SelectElement(Driver.FindElement(By.CssSelector("#calculator-pace > form > div:nth-child(4) > div > span > span > span.selectboxit-text")));
-
-        private IWebElement calculateButton => Driver.FindElement(By.ClassName("btn btn-medium-yellow calculate-btn"));
+        private IWebElement dropdownDistanceTypeElement => Driver.FindElement(By.XPath("//span//*[@class='selectboxit ignore_selectbox selectboxit-enabled selectboxit-btn'][@name='distance_type']"));
+        private IWebElement dropdownDistanceTypeKm => Driver.FindElement(By.XPath("//ul//li[@data-val='km'][contains(.,'Kilometers')]"));
+        private IWebElement dropdownSpeedPerHourTypeField => Driver.FindElement(By.XPath("//span//*[@class='selectboxit ignore_selectbox selectboxit-enabled selectboxit-btn'][@name='pace_type']"));
+        private IWebElement dropdownSpeedPerHourTypePerKm => Driver.FindElement(By.XPath("//span//*[@data-val='km'][contains(.,'per km')]"));
+        private IWebElement calculateButton => Driver.FindElement(By.CssSelector(".btn.btn-medium-yellow.calculate-btn"));
+        private IWebElement paceResultMin => Driver.FindElement(By.XPath("//input[@name='pace_minutes']"));
+        private IWebElement paceResultHours => Driver.FindElement(By.XPath("//input[@name='pace_hours']"));
+        private IWebElement paceResultSeconds => Driver.FindElement(By.XPath("//input[@name='pace_seconds']"));
+        private IWebElement resetButton => Driver.FindElement(By.XPath("//*[@class='reset']"));
         public ActivePage(IWebDriver webdriver) : base(webdriver) { }
+
+        public void NavigateToPage()
+        {
+            if (Driver.Url != PageAddress)
+                Driver.Url = PageAddress;
+        }
 
         public void CloseAdvertisement()
         {
             WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(5));
             wait.Until(d => popUpAdvertisement.Displayed);
             popUpAdvertisementCloseButton.Click();
+        }
+        public void AcceptCookies()
+        {
+            Cookie myCookie = new Cookie("OptanonAlertBoxClosed",
+                "2021-09-05T16:39:14.308Z",
+                ".active.com",
+                "/",
+                DateTime.Now.AddDays(5));
+            Driver.Manage().Cookies.AddCookie(myCookie);
+            Driver.Navigate().Refresh();
         }
 
         public void InsertRunningHours(string hours)
@@ -48,19 +63,36 @@ namespace Automatinis1.Page
         public void InsertRunningDistance(string distance)
         { runningDistance.SendKeys(distance); }
 
-        public void SelectRunningDistanceType(string distanceType)
+        public void SelectRunningDistanceKm()
         {
-            dropdownDistanceType.SelectByValue(distanceType);
+            IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
+            js.ExecuteScript("arguments[0].scrollIntoView();", dropdownDistanceTypeElement);
+            GetWait(10);
+            dropdownDistanceTypeElement.Click();
+            dropdownDistanceTypeKm.Click();
         }
 
-        public void SelectRunningSpeedPerHourType(string type)
+        public void SelectRunningSpeedPerHourTypeKmPerHour()
         {
-            dropdownSpeedPerHourType.SelectByText(type);
+            IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
+            js.ExecuteScript("arguments[0].scrollIntoView();", dropdownSpeedPerHourTypeField);
+            GetWait(10);
+            dropdownSpeedPerHourTypeField.Click();
+            dropdownSpeedPerHourTypePerKm.Click();
         }
 
-        public void CountSpeedPerHour()
+        public void CountPacePerHour()
         {
             calculateButton.Click();
+        }
+
+        public void ValidatePaceResult(string resultMin)
+        {
+            Assert.AreEqual(resultMin, paceResultMin.GetAttribute("value"), $"Speed result min is {paceResultMin.Text}, should be {resultMin}");
+        }
+        public void Reset()
+        {
+            resetButton.Click();
         }
 
 
