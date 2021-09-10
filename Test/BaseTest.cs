62b2a95b11c54eb6977ee377c7f241f7d5c2af96
LaukoGeliu <89358405+LaukoGeliu@users.noreply.github.com>
2021-09-10 14:53:16 +0300

    Add files via upload

---
+++ Test/BaseTest.cs
@@ -0,0 +1,43 @@
+﻿
+
+using MembershopTest.Drivers;
+using MembershopTest.Page;
+using MembershopTest.Tools;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace MembershopTest.Test
+{
+    public class BaseTest
+    {
+        public static IWebDriver driver;
+        public static MembershopHomePage _membershopHomePage;
+        public static MembershopSearchResultPage _membershopSearchResultPage;
+           
+
+        [OneTimeSetUp]
+        public static void SetUp()
+        {
+            driver = CustomDriver.GetChromeWithOptions();
+
+            _membershopHomePage = new MembershopHomePage(driver);
+           _membershopSearchResultPage = new MembershopSearchResultPage(driver);
+        }
+
+        [TearDown]
+        public static void TakeScreeshot()
+        {
+            if (TestContext.CurrentContext.Result.Outcome != ResultState.Success)
+                MyScreenshot.TakeScreenshot(driver);
+        }
+
+        [OneTimeTearDown]
+        public static void TearDown()
+        {
+            // driver.Quit();
+        }
+
+    }
+}
