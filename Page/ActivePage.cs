2054e79bb131ac20afebd126c547de2ef547d2d9
Gintarė <89358405+LaukoGeliu@users.noreply.github.com>
2021-09-15 18:39:28 +0300

    Add files via upload

--- Page/ActivePage.cs
+++ Page/ActivePage.cs
@@ -12,20 +12,16 @@ namespace MembershopTest.Page
 {
     public class ActivePage : BasePage
     {
-        private string distanceType;
-        private string paceType;
         private const string PageAddress = "https://www.active.com/fitness/calculators/pace";
         private IWebElement popUpAdvertisement => Driver.FindElement(By.ClassName("virtual-events-modal__inner"));
         private IWebElement popUpAdvertisementCloseButton => Driver.FindElement(By.ClassName("virtual-events-modal__close"));
-        //private IWebElement imageAdvertisement => Driver.FindElement(By.CssSelector("#img_anch_CL6kveG0yvICFchV4Aodq5cIjQ > img"));
-        // private IWebElement imageAdvertisementCLoseButton => Driver.FindElement(By.CssSelector("#cbb > svg"));
         private IWebElement runningTimeHours => Driver.FindElement(By.Name("time_hours"));
         private IWebElement runningTimeMinutes => Driver.FindElement(By.Name("time_minutes"));
         private IWebElement runningDistance => Driver.FindElement(By.Name("distance"));
         private IWebElement dropdownDistanceTypeElement => Driver.FindElement(By.XPath("//span//*[@class='selectboxit ignore_selectbox selectboxit-enabled selectboxit-btn'][@name='distance_type']"));
-        private IWebElement dropdownDistanceTypeKm => Driver.FindElement(By.XPath($"//ul//li[@data-val='km'][contains(.,'{distanceType}')]"));
+        private IWebElement dropdownDistanceTypeKm => Driver.FindElement(By.XPath("//ul//li[@data-val='km'][contains(.,'Kilometers')]"));
         private IWebElement dropdownSpeedPerHourTypeField => Driver.FindElement(By.XPath("//span//*[@class='selectboxit ignore_selectbox selectboxit-enabled selectboxit-btn'][@name='pace_type']"));
-        private IWebElement dropdownSpeedPerHourTypePerKm => Driver.FindElement(By.XPath($"//span//*[@data-val='km'][contains(.,'{paceType}')]"));
+        private IWebElement dropdownSpeedPerHourTypePerKm => Driver.FindElement(By.XPath("//span//*[@data-val='km'][contains(.,'per km')]"));
         private IWebElement calculateButton => Driver.FindElement(By.CssSelector(".btn.btn-medium-yellow.calculate-btn"));
         private IWebElement paceResultMin => Driver.FindElement(By.XPath("//input[@name='pace_minutes']"));
         private IWebElement paceResultHours => Driver.FindElement(By.XPath("//input[@name='pace_hours']"));
@@ -37,13 +33,6 @@ namespace MembershopTest.Page
         {
             if (Driver.Url != PageAddress)
                 Driver.Url = PageAddress;
-        }
-
-        public void CloseAdvertisement()
-        {
-            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(5));
-            wait.Until(d => popUpAdvertisement.Displayed);
-            popUpAdvertisementCloseButton.Click();
         }
         public void AcceptCookies()
         {
@@ -65,20 +54,14 @@ namespace MembershopTest.Page
         public void InsertRunningDistance(string distance)
         { runningDistance.SendKeys(distance); }
 
-        public void SelectRunningDistance(string distanceType)
+        public void SelectRunningDistanceKm()
         {
-            IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
-            js.ExecuteScript("arguments[0].scrollIntoView();", dropdownDistanceTypeElement);
-            GetWait(10);
             dropdownDistanceTypeElement.Click();
             dropdownDistanceTypeKm.Click();
         }
 
-        public void SelectRunningPacePerHourType(string paceType)
+        public void SelectRunningSpeedPerHourTypeKmPerHour()
         {
-            IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
-            js.ExecuteScript("arguments[0].scrollIntoView();", dropdownSpeedPerHourTypeField);
-            GetWait(10);
             dropdownSpeedPerHourTypeField.Click();
             dropdownSpeedPerHourTypePerKm.Click();
         }
