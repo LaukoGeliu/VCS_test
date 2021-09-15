fcf41cb9bb650995b52e982d37098167a69826aa
Gintarė <89358405+LaukoGeliu@users.noreply.github.com>
2021-09-15 18:40:19 +0300

    Add files via upload

--- Test/ActiveTest.cs
+++ Test/ActiveTest.cs
@@ -10,17 +10,16 @@ namespace MembershopTest.Test
 {
     public class ActiveTest : BaseTest
     {
-        [TestCase("1", "5", "13", "Kilometers", "per km", "05", TestName = "13 km in 1 hour 5 min is 5 km/hour")]
-        public static void Test13KmIn1Hour5Min(string hour, string minutes, string distance, string distanceType, string paceType, string resultMin)
+        [TestCase("1", "5", "13", "05", TestName = "13 km in 1 hour 5 min is 5 km/hour")]
+        public static void Test13KmIn1Hour5Min(string hour, string minutes, string distance, string resultMin)
         {
             _activePage.NavigateToPage();
-            //_activePage.CloseAdvertisement();
             _activePage.AcceptCookies();
             _activePage.InsertRunningHours(hour);
             _activePage.InsertRunningMinutes(minutes);
             _activePage.InsertRunningDistance(distance);
-           _activePage.SelectRunningDistance(distanceType);
-            _activePage.SelectRunningPacePerHourType(paceType);
+            _activePage.SelectRunningDistanceKm();
+            _activePage.SelectRunningSpeedPerHourTypeKmPerHour();
             _activePage.CountPacePerHour();
             _activePage.ValidatePaceResult(resultMin);
             _activePage.Reset();
